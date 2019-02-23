e6bf8059613f9144b8f94eb488dca01fa2604847
Pacze <dan.alexandru96@gmail.com>
2019-02-24 00:27:14 +0200

    Update

--- Savnet/src/TestEA16.java
+++ Savnet/src/TestEA16.java
@@ -1,32 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class TestEA16 {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 2, 3, 2, 3, 4, 1, 2, 4 };
-		int[] brr = new int[] { 1, 2 };
-		countAppearances(arr, brr);
+		int[] arr = new int[] { 1, 1, 2, 1, 2, 1, 1, 1 };
+		int[] brr = new int[] { 1, 2, 1 };
+		System.out.println(countAppearances(arr, brr));
+
 	}
 
 	public static int countAppearances(int[] a, int[] b) {
-		Arrays.sort(a);
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		int k = 0, nr = 0;
-		for (int j = 0; j < b.length; j++) {
-			for (int i = 0; i < a.length - 1; i++) {
-				if (a[i] == a[i + 1]) {
+		int i = 0, j = 0, nr = 0;
+
+		while (i < a.length && j < b.length) {
+			if (a[i] == b[j]) {
+				i++;
+				j++;
+				if (j == b.length) {
 					nr++;
+					if (b.length != 1) {
+						i--;
+					}
+					j = 0;
 
 				}
+			} else {
+
+				j = 0;
+				if (a[i] != b[j])
+					i++;
 			}
-			array.add(nr);
-			k++;
-
 		}
-
-		System.out.println(Arrays.toString(array.toArray()));
-		return 3;
-
+		return nr;
 	}
 }
