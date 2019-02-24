7ce6d439be8bd3dc4b39f40c547adaee8bfde630
Pacze <dan.alexandru96@gmail.com>
2019-02-24 16:57:50 +0200

    update2

--- Savnet/src/TestEA17.java
+++ Savnet/src/TestEA17.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TestEA17 {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 2, 3 };
-		removeDuplicates(arr);
+
+		System.out.println(Arrays.toString(removeDuplicates(new int[] { 1, 3, 2, 2, 2, 3, 3, 3, 3 })));
+
 	}
 
 	public static int[] removeDuplicates(int[] arr) {
 		int[] testarray = new int[] {};
 		if (arr.length == 0)
 			return testarray;
+
+		Arrays.sort(arr);
 
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		for (int i = 0; i < arr.length - 1; i++) {
