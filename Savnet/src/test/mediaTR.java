9489046b0c2bd6793fdf454963fc436b617c8552
paczeikadan <paczeikadan@192.168.1.32>
2019-03-19 18:35:58 +0200

    tr1

---
+++ Savnet/src/test/mediaTR.java
@@ -0,0 +1,16 @@
+package test;
+
+public class mediaTR {
+
+public static void main(String[] args) {
+        System.out.println(harmonicAvg(3,4));
+    }
+    public static double harmonicAvg(int a,int b)
+    {
+        
+        if (a+b==0)
+            return 0;
+      return (2*a*b)/(a+b);
+    }
+
+}
